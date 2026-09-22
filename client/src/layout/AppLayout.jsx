40a0c62287859f5a11de77bb5115aa97300cbbec
Prateek Kumar <sqonyx56@gmail.com>
2026-09-22 23:03:35 +0530

    testing axios and tanstack query

--- client/src/layout/AppLayout.jsx
+++ client/src/layout/AppLayout.jsx
@@ -1,7 +1,16 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
+import { useQuery } from "@tanstack/react-query";
+import { getMe } from "../api/api.auth";
 
 const AppLayout = () => {
+  useQuery({
+    queryKey: ["getMe"],
+    queryFn: getMe,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <div className="px-5 py-3">
       <Navbar />
