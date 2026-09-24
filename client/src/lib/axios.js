e405138c961745d4d26e3ecf573252e4062eb23f
Prateek Kumar <sqonyx56@gmail.com>
2026-09-24 23:21:36 +0530

    response interceptor

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -6,4 +6,33 @@ let api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    let originalRequest = error?.config;
+    let errMessage = error.response?.data?.message;
+    let errSuccess = error.response?.data?.success;
+    let errStatusCode = error.response?.status;
+
+    if (errSuccess) return Promise.reject(error);
+
+    if (errMessage === "Access token expired") {
+      if (errStatusCode !== 401 || originalRequest._retry)
+        return Promise.reject(error);
+      originalRequest._retry = true;
+ 
+      try {
+        await axios.get(`${API_URL}/auth/refreshToken`, {
+          withCredentials: true,
+        });
+        return api(originalRequest);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api;
